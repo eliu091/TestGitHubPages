0d0eb0b793b01d6929a990e89c5a44f97391dbd4
Eric Liu <eric.liu@pwc.com>
2018-03-05 18:19:42 +0800

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -9,4 +9,12 @@ $(document).ready(function () {
     $('#progress-bar-entity').css({display: 'none'})
     $('body').removeClass('noScroll')
   })
+
+  $('#exampleModal').off('shown.bs.modal').on('shown.bs.modal', function (e) {      
+      $('body').addClass('noScroll');
+  });
+
+    $('#exampleModal').off('hide.bs.modal').on('hide.bs.modal', function (e) {      
+      $('body').removeClass('noScroll');
+  });
 })
